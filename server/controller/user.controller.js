1bba4d0cd453580007b5fc522f8557854c18e5a6
Sudhir Kumar <sudhirkumarjnv2k12@gmail.com>
2025-01-26 15:53:20 +0530

    created editProfile controller

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,6 +1,8 @@
 import {User} from "../models/user.model.js"
 import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
+import getDataUri from "../utils/getDaraUri.js"
+import cloudinary from "../utils/cloudinary.js"
 
 export const register = async(req, res)=>{
     try {
@@ -106,5 +108,68 @@ export const logout = async(req, res)=>{
         })
     } catch (error) {
         console.log(error)
+    }
+}
+
+export const editProfile = async(req, res)=>{
+    try {
+        const userId = req.id;
+
+        let {name, bio, gender, workEducation, locations, contactInfo, familyRelationships} = req.body;
+        const profilePicture = req.files?.profilePicture?.[0];
+        const coverPicture = req.files?.coverPicture?.[0];
+
+        
+        if (workEducation && typeof workEducation === "string") {
+            workEducation = JSON.parse(workEducation);
+        }
+  
+        if (locations && typeof locations === "string") {
+            locations = JSON.parse(locations);
+        }
+    
+        if (contactInfo && typeof contactInfo === "string") {
+            contactInfo = JSON.parse(contactInfo);
+        }
+
+        const user = await User.findById(userId);
+        if(!user){
+            return res.status(401).json({
+                message:"error in fetching user details",
+                success:false,
+            })
+        }
+        
+        let cloudResponse1
+        if(profilePicture){
+            const fileUri = await getDataUri(profilePicture);
+            cloudResponse1 = await cloudinary.uploader.upload(fileUri);
+        }
+
+        let cloudResponse2
+        if(coverPicture){
+            const fileUri = getDataUri(profilePicture);
+            cloudResponse2 = cloudinary.uploader.upload(fileUri);
+        }
+
+        if(profilePicture)  user.profilePicture = await cloudResponse1.secure_url;
+        if(coverPicture)  user.cloudResponse2 = await cloudResponse2.secure_url;
+        if(name) user.name = name;
+        if(bio) user.bio = bio;
+        if(gender) user.gender = gender;
+        if(workEducation) user.workEducation = workEducation;
+        if(locations) user.locations = locations;
+        if(contactInfo) user.contactInfo = contactInfo;
+        if(familyRelationships) user.familyRelationships = familyRelationships;
+
+         await user.save()
+
+        return res.status(200).json({
+            message:"Account updated Successfully",
+            success:true,
+            user
+        })
+    } catch (error) {
+        console.log(error);
     }
 }
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,12 +1,15 @@
 import express from "express"
+import isAuthenticated from "../middleware/isAuthenticated.js"
+import upload from "../middleware/multer.js"
 
-import { register, login, logout} from "../controller/user.controller.js"
+import { register, login, logout, editProfile} from "../controller/user.controller.js"
 
 const router = express.Router();
 
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/logout').get(logout);
+router.route('/profile/editProfile').post(isAuthenticated, upload.fields([{name:'profilePicture', maxCount:'1'}, {name:'coverPicture', maxCount:'1'}]), editProfile);
 
 
 export default router;
