e93c016c44fc1d103868426e0289e3103245ace1
Sudhir Kumar <sudhirkumarjnv2k12@gmail.com>
2025-04-14 17:20:09 +0530

    ,

--- client/src/redux/ChatSlice.js
+++ client/src/redux/ChatSlice.js
@@ -86,6 +86,6 @@ const chatSlice = createSlice({
     },
 });
 
-export const { setSelectedChatType, setSelectedChatData, setSelectedChatMessages, addMessage, setReceivedMessage, setRecentChatList, updateRecentChatList, closeChat, recentUsersList } = chatSlice.actions;
+export const { setSelectedChatType, setSelectedChatData, setSelectedChatMessages,setGroups, addGroups, addMessage, setReceivedMessage, setRecentChatList, updateRecentChatList, closeChat, recentUsersList } = chatSlice.actions;
 
 export default chatSlice.reducer;
